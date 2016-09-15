3a3c96893c6741b6ad8b95e76690e11cc8b384c1
Saul <andjel@gmail.com>
2016-09-15 09:01:51 +0300

    added subnet options

--- scripts/00-ec2-launch-manual.sh
+++ scripts/00-ec2-launch-manual.sh
@@ -2,9 +2,11 @@
 #
 # Usage: $0 <config file>
 
-source scripts/helpers.sh
-
 XDCONFIG=$1
+
+source $XDCONFIG
+
+source scripts/helpers.sh
 
 if [ -n "$XDCONFIG" ]; then
     $aws s3 cp $XDCONFIG s3://$XDPRIV/etc/config
@@ -12,6 +14,8 @@ if [ -n "$XDCONFIG" ]; then
 
     #  created via IAM console: role/xd-scraper
     $aws ec2 run-instances \
+      --associate-public-ip-address \
+      --subnet-id ${SUBNET_ID} \
       --key-name $KEY \
       --region ${REGION} \
       --instance-type ${INSTANCE_TYPE} \
@@ -21,17 +25,18 @@ if [ -n "$XDCONFIG" ]; then
       --user-data file://scripts/01-ec2-thereafter.sh \
       --image-id ${AMI_ID} > $INSTANCE_JSON
 
+    instance_id=$(cat $INSTANCE_JSON | jq -r .Instances[0].InstanceId)
+    echo ${instance_id} started
     # Wait a little before applying security group
     sleep 30
-    instance_id=$(cat $INSTANCE_JSON | jq -r .Instances[0].InstanceId)
     $aws ec2 modify-instance-attribute --groups ${SSH_SECURITY_GID} --instance-id $instance_id
 
     # Manual root volume replacement should be done
-    current_vol_id=current_vol_id
-    VOLUME_ID=VOLUME_ID
-    $aws ec2 detach-volume --volume-id $current_volume_id
-    $aws ec2 attach-volume --volume-id $VOLUME_ID --instance-id $instance_id --device /dev/sda1
-    $aws ec2 delete-volume --volume-id $current_volume_id
+#    current_vol_id=$(aws ec2 describe-volumes --filters "Name=attachment.instance-id,Values=${instance_id}" | jq 
+#    VOLUME_ID=VOLUME_ID
+#    $aws ec2 detach-volume --volume-id $current_vol_id
+ #   $aws ec2 attach-volume --volume-id $VOLUME_ID --instance-id $instance_id --device /dev/sda1
+#    $aws ec2 delete-volume --volume-id $current_vol_id
 
     public_ip=$(aws ec2 describe-instances --instance-ids ${instance_id} | jq -r '.Reservations[0].Instances[0].PublicIpAddress')
     echo "Connect to ${instance_id} :  ssh -i ~/*.pem ubuntu@$public_ip"
